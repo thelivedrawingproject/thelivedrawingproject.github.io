035cad3b1bfb7d1be9403f56bf740012801eee96
Maxime Touroute <livedrawingproject@protonmail.com>
2019-03-13 19:46:26 +0100

    quickfix 404 page causing infinite loop

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import MainLayout from '../layout/MainLayout'
 import { notFoundPage } from '../locales/strings'
+import { Navbar } from '../components/Navbar'
+import { NavbarData } from '../layout/NavbarData'
+import Footer from '../components/Footer'
 
-export default function NotFoundPage({ data, pageContext: { locale }, location }) {
+// TODO: dirty copy paste of MainLayout to avoid crazy language switcher (because URL doesnt exist)
+
+export default function NotFoundPage({ data, pageContext: { locale } }) {
   const LOCAL = notFoundPage[locale];
 
   return (
-    <MainLayout language={locale} location={{ ...location }}>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: '4rem',
-      }}>
-        <h1>{LOCAL.title}</h1>
-        <p>{LOCAL.subtext}</p>
+    <>
+      <div className="CssGridNavContentFooter">
+        <nav className="gridNavBar">
+          <Navbar links={NavbarData[locale].links}/>
+        </nav>
+        <div className="gridContent styleContent">
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: '4rem',
+          }}>
+            <h1>{LOCAL.title}</h1>
+            <p>{LOCAL.subtext}</p>
+          </div>
+        </div>
+        <div className="gridFooter">
+          <Footer/>
+        </div>
       </div>
-    </MainLayout>
+    </>
   )
 }
