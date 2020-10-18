2ac561070f8a3ced3c522d9c48d41cc53a989bd7
Maxime Touroute <maxime.touroute@gmail.com>
2020-10-18 10:56:31 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bc7655660e07101ac79a.js"
+    "url": "webpack-runtime-0caef9efdcd661a870da.js"
   },
   {
     "url": "framework-4b6e6fca25677e0206fe.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fff0f1288268db92c4f8a6f84bc31dd0"
+    "revision": "b23f5306ee3174d9e964214808153b88"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-859e22a7cba0ed2e01d8.js"
