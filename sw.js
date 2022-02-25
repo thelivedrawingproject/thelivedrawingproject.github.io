ed6c491a35168874a124f664206c38ea3ff63499
Maxime Touroute <livedrawingproject@protonmail.com>
2022-02-25 22:10:12 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-edf1ce81891b08c99dc2.js"
+    "url": "webpack-runtime-00a21dec343ad4ce3bac.js"
   },
   {
     "url": "framework-7e54c0873f66b5bc9c9c.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3932873fae2b77a53909ab98b2dfe9fd"
+    "revision": "276295a500031c904cc5ba598f24dcea"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-dd23be24b87cd81828a2.js"
